99807d9caa5143cc3f4072061ae89bcef90624cc
Michael Bestas <mkbestas@lineageos.org>
2023-03-03 23:43:42 +0200

    Build Lineage Health HAL

--- device.mk
+++ device.mk
@@ -55,6 +55,10 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
