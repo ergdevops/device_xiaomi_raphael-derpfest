62824af54c84d2b49b82e1033bacdcf1b4e94e9f
Master <mhmdeveloperofficial@gmail.com>
2024-02-07 00:21:29 +0900

    raphael: Add ViPER4Android FX support
    Signed-off-by: kaderbava <ksbava7325@gmail.com>
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- device.mk
+++ device.mk
@@ -629,6 +629,9 @@ PRODUCT_PACKAGES += \
 # Vibrator
 $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
+# Viper
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # VNDK
 PRODUCT_PACKAGES += \
     libcrypto-v33 \
