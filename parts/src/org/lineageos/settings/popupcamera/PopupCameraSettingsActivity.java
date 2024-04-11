8fd15a0c4095d56762febfcfe8ceea377959a014
ergdevops <honor6firmware@gmail.com>
2024-04-11 12:42:52 +0000

    adapt qpr2 changes

--- parts/src/org/lineageos/settings/popupcamera/PopupCameraSettingsActivity.java
+++ parts/src/org/lineageos/settings/popupcamera/PopupCameraSettingsActivity.java
@@ -20,7 +20,7 @@ package org.lineageos.settings.popupcamera;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
+//import com.android.settingslib.widget.R;
 
 public class PopupCameraSettingsActivity extends CollapsingToolbarBaseActivity {
     private static final String TAG_POPUPCAMERA = "popupcamera";
@@ -31,7 +31,7 @@ public class PopupCameraSettingsActivity extends CollapsingToolbarBaseActivity {
 
         getFragmentManager()
                 .beginTransaction()
-                .replace(R.id.content_frame, new PopupCameraSettingsFragment(), TAG_POPUPCAMERA)
+                .replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame, new PopupCameraSettingsFragment(), TAG_POPUPCAMERA)
                 .commit();
     }
 }
