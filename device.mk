a52060a3d440d0295c998773d71b68b02ab18c7d
ergdevops <honor6firmware@gmail.com>
2024-04-28 20:07:23 +0530

    raphael: Move UDFPS Radius overlay to Setting

--- device.mk
+++ device.mk
@@ -64,6 +64,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-derp
 
+PRODUCT_PACKAGES += \
+    RaphaelSettings
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
