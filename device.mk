1b89406cea30547931b1a6c4a5fbe24ccff4d3bf
ergdevops <honor6firmware@gmail.com>
2024-06-27 23:15:10 +0530

    added missing dependencies

--- device.mk
+++ device.mk
@@ -141,6 +141,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    android.hardware.camera.provider@2.5 \
     libdng_sdk.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor \
@@ -429,7 +430,7 @@ PRODUCT_PACKAGES += \
 # QMI
 PRODUCT_PACKAGES += \
     libcurl.vendor \
-    libjson \
+    	libjson \
     libjsoncpp.vendor
 
 # QTI
@@ -617,5 +618,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Missing Vendor Packages
 PRODUCT_PACKAGES += \
-    libutilscallstack.vendor \
-    libdumpstateutil.vendor
+    libutilscallstack.vendor:64 \
+    libdumpstateutil.vendor \
+    libion.vendor \
+    libnetutils.vendor:64 \
+    libssl.vendor \
+    libpower.vendor \
+    liba2dpoffload \
+    libldacBT_bco \
+    libstagefright_softomx_plugin.vendor \
+    libprocessgroup.vendor
+
+PRODUCT_EXTRA_VNDK_VERSIONS := 30
+
