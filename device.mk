2ad0e31bb26abd1bf68a0a9589bd091471f94edc
kondors1995 <normandija1945@gmail.com>
2024-07-27 11:25:30 +0300

    raphael:remove nonexistant targets

--- device.mk
+++ device.mk
@@ -115,7 +115,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-util \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.audio.sounddose-vendor-impl \
     android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.bluetooth.default \
@@ -641,7 +640,6 @@ PRODUCT_PACKAGES += \
     libprocessgroup.vendor \
     libdumpstateutil.vendor \
     libjsoncpp.vendor \
-    llibpng.vendor \
     libcurl.vendor \
     libpng.vendor:32
 
