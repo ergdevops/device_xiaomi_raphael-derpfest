e501f1a40e67906c79a75f96bdabdfb912157457
Jyotiraditya Panda <jyotiraditya@aospa.co>
2024-08-06 06:29:37 +0530

    common: Build android.hidl.memory.block@1.0.vendor
     - Required by qcom.trustedui and qcom.eid
    
    Change-Id: I3dfbab252ea539bdacac67a75e5d8a9b7d889ad0
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- device.mk
+++ device.mk
@@ -619,6 +619,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libtinyxml2
 
+# Trusted User Interface
+PRODUCT_PACKAGES += \
+    android.hidl.memory.block@1.0.vendor
+
 # USB
 PRODUCT_PACKAGES += \
     usb_compositions.conf \
