b4223a6bd30f4b6e56ff1c14d61a06fc4390fe30
ergdevops <honor6firmware@gmail.com>
2024-10-30 23:39:38 +0530

    raphael: add remove packages

--- device.mk
+++ device.mk
@@ -679,3 +679,8 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 # Enable zygote critical window.
 PRODUCT_PROPERTY_OVERRIDES += \
     zygote.critical_window.minute=10
+
+
+# Remove packages
+PRODUCT_PACKAGES += \
+    RemovePackages
