07c80faa6fb6a73c5a5b13134a40b560c3fef9e7
Ubuntu <honor6firmware@gmail.com>
2024-11-14 10:28:54 +0000

    raphael: derpfest device bringup

---
+++ derp_raphael.mk
@@ -0,0 +1,56 @@
+# Copyright (C) 2020 LineageOS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/xiaomi/raphael/device.mk)
+
+# Inherit some common DerpFest stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Charging Animation
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+# Disable/Enable Blur Support, default is false
+TARGET_ENABLE_BLUR := true
+
+# Derp
+DERP_BUILDTYPE := Official
+#TARGET_USES_PICO_GAPPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+DERP_VERSION_APPEND_TIME_OF_DAY := true
+
+# Quick Tap Feature
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := derp_raphael
+PRODUCT_DEVICE := raphael
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 9T Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="raphael"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+BUILD_FINGERPRINT := Xiaomi/raphael/raphael:11/RKQ1.200826.002/V12.5.2.0.RFKMIXM:user/release-keys
