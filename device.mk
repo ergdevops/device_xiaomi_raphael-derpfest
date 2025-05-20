096fcce60d40ac1b5d4f033647a7c0c8380dfa27
Ubuntu <honor6firmware@gmail.com>
2025-05-20 17:53:37 +0000

    Revert "raphael: Build DisplayFeatures"
    This reverts commit 896668b1fd3dea54763a673691521746699a5994.

--- device.mk
+++ device.mk
@@ -251,11 +251,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey
 
-# DisplayFeatures
-PRODUCT_PACKAGES += \
-    DisplayFeatures \
-    DisplayFeaturesRaphael
-
 # F2FS
 PRODUCT_PACKAGES += \
     sg_write_buffer \
